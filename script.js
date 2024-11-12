b6b21576d82f9fc677c926f5681549c4b54513e0
vivekv88 <166617357+vivekv88@users.noreply.github.com>
2024-11-12 23:28:26 +0530

    upload files

---
+++ script.js
@@ -0,0 +1,21 @@
+function handleclick(value){
+    document.getElementById('display').value += value
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = ''
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
